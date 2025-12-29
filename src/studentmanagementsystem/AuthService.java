10b8385a1c0956a5d5f9fd96dc909ead8f2b9089
samTime101 <srijanregmi44@gmail.com>
2025-12-29 13:13:40 +0545

    Auth service added

---
+++ src/studentmanagementsystem/AuthService.java
@@ -0,0 +1,11 @@
+package studentmanagementsystem;
+
+public class AuthService {
+	public boolean authenticate(String username, String password) {
+		return false;
+	}
+	
+	public boolean changePassword(int userId, String oldPassword, String newPassword) {
+		return false;
+	}
+}
