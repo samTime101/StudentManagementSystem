776722b60bb637cc2f42234a8e4f76498488acf1
Kshitiz Shrestha <kshitijxtha3646@icloud.com>
2025-12-29 13:55:49 +0545

    changes

---
+++ src/marks/Marks.java
@@ -0,0 +1,11 @@
+package marks;
+
+public class Marks {
+    private int marksId;
+    private int studentId;
+    private int courseId;
+    private double marks;
+    private String grade;
+
+    // getters and setters later
+}
---
+++ src/marks/MarksDAO.java
@@ -0,0 +1,20 @@
+package marks;
+
+public class MarksDAO {
+
+    public boolean addMarks(Marks marks) {
+        return true;
+    }
+
+    public boolean updateMarks(Marks marks) {
+        return true;
+    }
+
+    public Marks getMarks(int studentId, int courseId) {
+        return null;
+    }
+
+    public Marks getMarksByStudent(int studentId) {
+        return null;
+    }
+}
